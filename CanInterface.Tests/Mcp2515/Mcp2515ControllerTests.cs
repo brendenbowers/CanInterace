77bd2a6e8220d9caa85f5a8a0467e2cce7ceccd0
brenden bowers <crazy560@hotmail.com>
2017-02-04 18:21:01 -0600

    added some new tests for the mcp2515 controller

---
+++ CanInterface.Tests/Mcp2515/Mcp2515ControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using CanInterface.MCP2515;
+using CanInterface.MCP2515.Enum;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanInterface.Tests.Mcp2515
+{
+    [TestFixture(Category = "Unit Test")]
+    public class Mcp2515ControllerTests
+    {
+        protected Mock<IDevice> Device;
+        protected Mcp2515Controller Controller;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            Device = new Mock<IDevice>();
+            Controller = new Mcp2515Controller(Device.Object);
+        }
+
+
+        [TestCase(Registers.BFPCTRL,  (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - BFPCTRL")]
+        [TestCase(Registers.EFLG,     (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - EFLG")]
+        [TestCase(Registers.CANSTAT,  (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - CANSTAT")]
+        [TestCase(Registers.CANCTRL,  (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - CANCTRL")]
+        [TestCase(Registers.CANINTE,  (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - CANINTE")]
+        [TestCase(Registers.CANINTF,  (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - CANINTF")]
+        [TestCase(Registers.TXB0CTRL, (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - TXB0CTRL")]
+        [TestCase(Registers.TXB1CTRL, (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - TXB1CTRL")]
+        [TestCase(Registers.TXB2CTRL, (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - TXB2CTRL")]
+        [TestCase(Registers.RXB0CTRL, (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - RXB0CTRL")]
+        [TestCase(Registers.RXB1CTRL, (byte)0b0001_0001, TestName = "Mcp2515ControllerReadRegisterData - RXB1CTRL")]
+        public void Mcp2515ControllerReadRegister(byte register, byte expectedOutput)
+        {
+            byte[] inputData = null;
+            Device.Setup(m => m.Write(It.IsAny<byte[]>(), It.IsAny<byte[]>()))
+                .Callback<byte[], byte[]>(
+                (input, output) => {
+                    inputData = input;
+                    output[0] = expectedOutput;
+                });
+
+            Assert.That(() => Controller.ReadRegister(register), Is.EqualTo(expectedOutput));
+            Assert.That(inputData, Is.EqualTo(new[] { Commands.READ, register }));
+        }
+
+        [TestCase(Registers.BFPCTRL, new byte[] {1, 0, 1 }, TestName = "Mcp2515ControllerWriteRegister - BFPCTRL")]
+        [TestCase(Registers.CANINTF, new byte[] { 1, 1, 1 }, TestName = "Mcp2515ControllerWriteRegister - CANINTF")]
+        [TestCase(Registers.CANCTRL, new byte[] { 1, 1, 0 }, TestName = "Mcp2515ControllerWriteRegister - CANCTRL")]
+        public void Mcp2515ControllerWriteRegister(byte register, byte[] values)
+        {
+            byte[] expected = new byte[values.Length + 2];
+            expected[0] = Commands.WRITE;
+            expected[1] = register;
+            Array.ConstrainedCopy(values, 0, expected, 2, values.Length);
+
+            byte[] dataWritten = null;
+            Device.Setup(m => m.Write(It.IsAny<byte[]>())).Callback<byte[]>((toWrite) => dataWritten = toWrite);
+
+            Assert.That(() => Controller.WriteRegister(register, values), Throws.Nothing);
+            Assert.That(dataWritten, Is.EqualTo(expected));
+        }
+    }
+}
--- CanInterface.Tests/Properties/AssemblyInfo.cs
+++ CanInterface.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CanInterface.Test")]
+[assembly: AssemblyTitle("CanInterface.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CanInterface.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("CanInterface.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d31131df-ec48-49b9-bdbc-8153585fb387")]
+[assembly: Guid("7d9bf195-8196-43ef-86f8-faa47b6c77c3")]
 
 // Version information for an assembly consists of the following four values:
 //
